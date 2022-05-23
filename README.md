e93bfc8402d390802aad662ef822be0e9dae00a3
root <root@localhost.localdomain>
2022-05-23 20:29:12 +0200

    Second commit / small bugfixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Phoenix
+
+Phoenix is a utilitary that brings back from ashes your removed files
--- phoenix.sh
+++ phoenix.sh
@@ -2,8 +2,32 @@
 
 # incron IN_DELETE
 
+# Declaration of variables
+
+rhel_release=$(cat /etc/redhat-release)
+ubuntu_release=$(cat /etc/os-release)
+
+
+if [ ! -e /bin/vim ], then 
+    yum install vim -y 
+fi
+
+# Check the os to know what packet manager we'll use to downolad incron
+
+if [ ! -e /etc/incron.d ]; then
+    yum install epel-release -y
+    yum install incron -y
+fi    
+
+# We create the log file if it does not exist 
+
 if [ ! -e /var/log/phoenix.log ]; then
     touch /var/log/phoenix.log
 fi
 
-echo /home/ilogeais/phoenix IN_DELETE /home/ilogeais/wphoenix.sh >> /etc/incron.d/incron 
+# We configure the incron to execute another script when the files are deleted
+
+echo $USER >> /etc/incron.allow
+
+# definitive version echo /home/$USER/* IN_DELETE /home/ian/wphoenix.sh >> /etc/incron.d/incron
+echo /home/ian/* IN_DELETE /home/ian/wphoenix.sh >> /etc/incron.d/incron
