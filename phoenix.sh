9ddcf1d17b0f33dbc54db2018d17a748680794e1
Ian Logeais <71567996+ianlogeais@users.noreply.github.com>
2022-05-23 18:53:34 +0200

    Update phoenix.sh

--- phoenix.sh
+++ phoenix.sh
@@ -1,1 +1,9 @@
+#!/bin/bash 
 
+# incron IN_DELETE
+
+if [ ! -e /var/log/phoenix.log ]; then
+    touch /var/log/phoenix.log
+fi
+
+echo /home/ilogeais/phoenix IN_DELETE /home/ilogeais/wphoenix.sh >> /etc/incron.d/incron 
