94ce8e03c2fd88d8fb035bcbd144849f58924919
Ian Logeais <71567996+ianlogeais@users.noreply.github.com>
2022-05-23 18:53:52 +0200

    Update wphoenix.sh

--- wphoenix.sh
+++ wphoenix.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+removed_file=$(fc -nl $((HISTCMD - 1)) | cut -d " " -f3)
+
+echo $removed_file >> /var/log/phoenix.log
+
+if [ ! -e /etc/phoenix ]; then
+    mkdir /etc/phoenix
+fi
+
+cp $removed_file /etc/phoenix/ 
